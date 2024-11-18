4d0c14b8c623d497e5d6628583c5ebe6dd88724d
IgorNeres <igoritalo9698@gmail.com>
2024-11-18 08:51:57 -0300

    Delete comandos directory

--- comandos/read.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include 'connection.php';
-    $listaSQL_clie = mysqli_query($connectionBD, "SELECT * FROM cliente ORDER BY nome_clie ASC");
-    $listaSQL_prod = mysqli_query($connectionBD, "SELECT * FROM produto");
-?>
